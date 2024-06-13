c171f185a938c5c6e8827296c0ad92f6805eacee
Neelanjan V <137300097+chatbca@users.noreply.github.com>
2024-06-13 15:56:32 +0530

    Update b7Linklist.java

--- b7Linklist.java
+++ b7Linklist.java
@@ -1,24 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package b7.linklist;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-/**
- *
- * @author HP
- */
 public class B7LinkList {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         LinkedList<Integer> flist = new LinkedList<Integer>();
         LinkedList<Integer> slist = new LinkedList<Integer>();
